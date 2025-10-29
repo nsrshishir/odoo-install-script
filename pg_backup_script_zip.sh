8942e465ccd1770ba05a24ebb371c45597988402
Niaj Shishir <nsrshishir550@gmail.com>
2025-10-29 12:45:44 +0600

    Checking for if zip package is installed added

--- pg_backup_script_zip.sh
+++ pg_backup_script_zip.sh
@@ -19,13 +19,13 @@ abs_bkup_filename=$src_bkup_loc$bkup_filename".zip"
 src_filestore_bak_loc=$src_bkup_loc"filestore"
 
 #Destination Host information
-dest_bkup_loc="/dest/db_backup/"
-dest_filestore=$dest_bkup_loc$db_name"_filestore"
+dest_bakeup_loc="/dest/db_backup/"
+dest_filestore=$dest_bakeup_loc$db_name"_filestore"
 dest_host="Host_address"
 dest_user="Username"
 dest_pass="Password"
 
-#Checking Source backup Directory, Creating one if doesnt exits
+#Checking Source backup Directory, Creating one if doesnt exists
 if [ ! -d $src_bkup_loc ]; then
     echo "Directory "$src_bkup_loc" does not exist! Creating..."
     mkdir $src_bkup_loc
@@ -34,14 +34,19 @@ if [ ! -d $src_bkup_loc ]; then
 fi
 
 if [ $daylimit -ne 0 ]; then
-    find $src_bkup_loc -type f -mtime +$daylimit -delete
+    find $src_bakeup_loc -type f -mtime +$daylimit -delete
 fi
 
-#Checking Source backup file exist, Removing one if exits
+#Checking Source backup file exist, Removing one if exists
 if test -f $abs_bkup_filename; then
     rm $abs_bkup_filename
 fi
 
+#Checking if zip package is installed
+if ! dpkg -s zip; then
+    echo "Zip package is not installed, installing..."
+    sudo apt install zip -y
+fi
 
 #delete filestore folder if exists in source backup location if exists
 if test -f $src_filestore_bak_loc; then
@@ -49,28 +54,25 @@ if test -f $src_filestore_bak_loc; then
 fi
 
 #dumping database
-sudo -u $db_user pg_dump --no-owner --no-privileges -f $src_bkup_loc$db_dump_filename $db_name
+sudo -u $db_user pg_dump --no-owner --no-privileges -f $src_bakeup_loc$db_dump_filename $db_name
 
 #Copping filestore
 cp -r $src_filestore_loc$db_name $src_filestore_bak_loc
 
-
 #Creating a tar.gz file with db dump and db filestores
-# tar -czf $abs_bkup_filename -C $src_bkup_loc $db_dump_filename -C $src_filestore_loc $db_name
-cd $src_bkup_loc
+cd $src_bakeup_loc
 zip -r $abs_bkup_filename $db_dump_filename filestore
 
 #removing dump.sql and filestores
-sudo rm $src_bkup_loc$db_dump_filename
+sudo rm $src_bakeup_loc$db_dump_filename
 sudo rm -r $src_filestore_bak_loc
-
-
 
 if [ $dest_user != "Host_address" ] && [ $dest_user != "Username" ] && [ $dest_pass != "Password" ]; then
     # sudo apt install sshpass
     #syncing databases
-    rsync -a --delete -e "sshpass -p $dest_pass ssh -o StrictHostKeyChecking=no" $src_bkup_loc $dest_user@$dest_host:$dest_bkup_loc
+    rsync -a --delete -e "sshpass -p $dest_pass ssh -o StrictHostKeyChecking=no" $src_bakeup_loc $dest_user@$dest_host:$dest_bakeup_loc
 
     #syncing filestore
     # rsync -a -e "sshpass -p $dest_pass ssh -o StrictHostKeyChecking=no" $src_filestore $dest_user@$dest_host:$dest_filestore
 fi
+
